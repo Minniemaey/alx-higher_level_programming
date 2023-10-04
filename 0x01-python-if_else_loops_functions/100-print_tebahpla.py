9a36f31cd3945eec2643f9625a7a85892a3220e4
Minniemaey <ayodedeyi@gmail.com>
2023-10-03 17:50:25 -0700

    task 100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for c in range(122, 96, -1):
-        if (c % 2 == 0):
-            c = chr(c)
-        else:
-            c = chr(c - 32)
-        print("{}".format(c), end="")
+    if (c % 2 == 0):
+        c = chr(c)
+    else:
+        c = chr(c - 32)
+    print("{}".format(c), end="")
