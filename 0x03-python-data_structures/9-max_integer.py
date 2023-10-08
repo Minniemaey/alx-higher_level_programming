b32214156a33604f34f33eaf0287934bfaa3d111
Minniemaey <ayodedeyi@gmail.com>
2023-10-08 03:09:46 -0700

    task 9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if (not my_list) or (len(my_list) == 0):
+        return None
+    max = my_list[0]
+    for num in my_list:
+        if num > ma:
+            max = num
+    return max
