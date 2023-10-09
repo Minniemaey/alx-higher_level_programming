2653300675e471911283ece6c71305f859cbf8a5
Minniemaey <ayodedeyi@gmail.com>
2023-10-09 04:34:18 -0700

    task 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -6,44 +6,40 @@
  * Return: 1 if palindrome else 0
  */
 
+#include "lists.h"
+
+/**
+ * is_palindrome - check if linked list is palindromic
+ * @head: double pointer to list head node
+ * Return: 1 if palindrome else 0
+ */
+
 int is_palindrome(listint_t **head)
 {
-	listint_t *ptr1 = *head, *ptr2 = *head, *curr, *prev;
-	int palin = 0;
+	listint_t *tmp = *head;
+	int *arr = malloc(100000 * sizeof(int));
+	int i = 1, arr_size = sizeof(int);
+	int palin1 = 0, palin2 = i - 1;
 
-	while (ptr2 != NULL && ptr2->next != NULL)
+	arr[0] = (*head)->n;
+
+	if (*head == NULL)
+		return (1);
+
+	tmp = tmp->next;
+
+	while (tmp != NULL)
 	{
-		ptr2 = ptr2->next->next;
-		ptr1 = ptr1->next;
+		arr_size += sizeof(int);
+		arr = realloc(arr, arr_size);
+		arr[i] = tmp->n;
+		i++;
+		tmp = tmp->next;
 	}
-	curr = ptr1;
-	prev = NULL;
-	while (curr)
+	for (; palin1 < palin2; palin1++, palin2--)
 	{
-		ptr2 = curr->next;
-		curr->next = prev;
-		prev = curr;
-		curr = fast;
+		if (arr[palin1] != arr[palin2])
+			return (0);
 	}
-	ptr2 = *head;
-	curr = prev;
-	while (prev)
-	{
-		if (ptr2->n != prev->n)
-		{
-			palin = 1;
-			break;
-		}
-		ptr2 = ptr2->next;
-		prev = prev->next;
-	}
-	prev = NULL;
-	while (curr)
-	{
-		ptr2 = curr->next;
-		curr->next = prev;
-		prev = curr;
-		curr = ptr2;
-	}
-	return (!palin);
+	return (1);
 }
