f0d746e02947af55cf7239f6ccd082fd7fbd26ed
Minniemaey <ayodedeyi@gmail.com>
2023-10-09 03:38:44 -0700

    task 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,58 +1,37 @@
 #include "lists.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
- * reverse_listint - Reverses a linked list
- * @head: Pointer to list head node
- *
- * Return: new head of the reversed list
- */
-
-listint_t *reverse_listint(listint_t **head)
-{
-	listint_t *prev = NULL, *curr = *head, *next = NULL;
-
-	while (curr != NULL)
-	{
-		next = curr->next;
-		curr->next = prev;
-		prev = curr;
-		curr = next;
-	}
-	*head = prev;
-	return (*head);
-}
-
-/**
- * is_palindrome - Checks if a linked list is a palindrome
+ * is_palindrome - check if linked list is palindromic
  * @head: double pointer to list head node
  * Return: 1 if palindrome else 0
  */
 
 int is_palindrome(listint_t **head)
 {
-	if (*head == NULL || (*head)->next == NULL)
+	listint_t *tmp = *head;
+	int *arr = malloc(100000 * sizeof(int));
+	int i = 1, arr_size = sizeof(int);
+	int palin1 = 0, palin2 = i - 1;
+
+	arr[0] = (*head)->n;
+
+	if (*head == NULL)
 		return (1);
 
-	listint_t *slow = *head, *fast = *head;
+	tmp = tmp->next;
 
-	while (fast->next != NULL && fast->next->next != NULL)
+	while (tmp != NULL)
 	{
-		slow = slow->next;
-		fast = fast->next->next;
+		arr_size += sizeof(int);
+		arr = realloc(arr, arr_size);
+		arr[i] = tmp->n;
+		i++;
+		tmp = tmp->next;
 	}
-	slow->next = reverse_listint(&(slow->next));
-
-	listint_t *palin1 = *head;
-	listint_t *palin2 = slow->next;
-
-	while (palin2 != NULL)
+	for (; palin1 < palin2; palin1++, palin2--)
 	{
-		if (palin1->n != palin2->n)
+		if (arr[palin1] != arr[palin2])
 			return (0);
-		palin1 = palin1->next;
-		palin2 = palin2->next;
 	}
 	return (1);
 }
