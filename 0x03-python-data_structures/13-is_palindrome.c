0b285c4b8bd2c85f25ca26672b17327a673d6617
Minniemaey <ayodedeyi@gmail.com>
2023-10-09 04:00:19 -0700

    task 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -10,28 +10,38 @@ int is_palindrome(listint_t **head)
 {
 	listint_t *tmp = *head;
 	int *arr = malloc(100000 * sizeof(int));
-	int i = 1, arr_size = sizeof(int);
+	int i = 1, arr_size = 100000;
 	int palin1 = 0, palin2 = i - 1;
 
 	arr[0] = (*head)->n;
 
 	if (*head == NULL)
+		free(arr);
 		return (1);
 
 	tmp = tmp->next;
 
 	while (tmp != NULL)
 	{
-		arr_size += sizeof(int);
-		arr = realloc(arr, arr_size);
+		if (i >= arr_size)
+		{
+			arr_size *= 2;
+			arr = realloc(arr, arr_size * sizeof(int));
+		}
 		arr[i] = tmp->n;
 		i++;
 		tmp = tmp->next;
 	}
-	for (; palin1 < palin2; palin1++, palin2--)
+	while (palin1 < palin2)
 	{
 		if (arr[palin1] != arr[palin2])
+		{
+			free(arr)
 			return (0);
+		}
+		palin1++;
+		palin2--;
 	}
+	free(arr)
 	return (1);
 }
