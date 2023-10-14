eb148e5c875c813899ebcf6cca5f49481f736e9c
Minniemaey <ayodedeyi@gmail.com>
2023-10-13 21:45:22 -0700

    task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,11 +1,37 @@
 #!/usr/bin/python3
+def calc(new_list):
+    numcount = 0
+    max_list = max(new_list)
+
+    for n in new_list:
+        if max_list > n:
+            numcount += n
+
+    return (max_list - numcount)
+
+
 def roman_to_int(roman_string):
-    if (not isinstance(roman_string, str) or roman_string is None):
+    if not roman_string or not isinstance(roman_string, str):
         return 0
-    fulnum = 0
-    num = 0
+
     numeral = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    for n in reversed(roman_string):
-        num = numeral[n]
-        fulnum = fulnum + num if fulnum < num * 5 else -num
-    return fulnum
+    list_keys = list(numeral.keys())
+
+    num = 0
+    digitn = 0
+    new_list = [0]
+
+    for char in roman_string:
+        for r_num in list_keys:
+            if r_num == char:
+                if numeral.get(char) <= digitn:
+                    num += calc(new_list)
+                    new_list = [numeral.get(char)]
+                else:
+                    new_list.append(numeral.get(char))
+
+                digitn = numeral.get(char)
+
+    num += calc(new_list)
+
+    return (num)
